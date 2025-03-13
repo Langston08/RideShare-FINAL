63b0a66170e72eb117ff063419b8adbbbb148a5f
Langston08 <langston.standridge26@students.polytechnic.org>
2025-03-13 16:35:07 -0700

    tester

--- RideshareTester.java
+++ RideshareTester.java
@@ -1,15 +1,16 @@
 public class RideshareTester {
     public static void main(String[] args) {
+        //start of rideshare!!!
         System.out.println("Completed Rideshare Project!");
         System.out.println();
         Road r = new Road(20, 50); //change to 40 if you want to see 40 passengers and 20 cars!
         System.out.println(r);
 
         while(r.getCars().size()>0){
-            r.move();
+            r.move(); //move along the road
         }
 
         System.out.println(r);
     }
 }
-
+//done with tester
