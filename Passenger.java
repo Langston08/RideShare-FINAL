d0ffbc030f7a83da374a879eed12bffaf341484b
Langston08 <langston.standridge26@students.polytechnic.org>
2025-03-13 16:29:50 -0700

    acknowledgement/comments

--- Passenger.java
+++ Passenger.java
@@ -1,26 +1,27 @@
 public class Passenger{
-    /* fields */
+    //fields
     private int dest;
     private int ID;
     private static int idgen;
 
-    /* constructors */
+    //constructors
     public Passenger(int mydest){
-        dest = mydest;
-        ID = idgen;
-        idgen++;
+        dest = mydest; 
+        ID = idgen; //assigning id to passenger
+        idgen++; //keep going up
     }
 
-    /* methods */
+    //methods
     public int getDes(){
-        return dest;
+        return dest; //return the destination station of the pasenger
     }
 
     public int getID(){
-        return ID;
+        return ID; //return the id of the passenger
     }
 
     public String toString(){
         return "Passenger " + ID + " | Destination: " + dest;
-    }
+    } //return a string of the passenger and their info
+    //AGAIN THANK YOU EVAN CHEN FOR THE FORMATTING
 }
